d37bd2680c28399339750fec8d06abca9cf27b6c
Artur <khalikart@gmail.com>
2021-03-26 17:13:57 +0500

    added review ch11

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -6,8 +7,7 @@ class Book(models.Model):
     id = models.UUIDField(
         primary_key=True,
         default = uuid.uuid4,
-        editable=False
-    )
+        editable=False)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -16,5 +16,14 @@ class Book(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('book_detail', args=[str(self.id)])
+        return reverse('book_detail', kwargs={'pk' : str(self.pk)})
 
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name='reviews',)
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return self.review
