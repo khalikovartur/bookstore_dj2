c3b165c379f86deedeeba706fa195c5a26641d55
Artur <khalikart@gmail.com>
2021-03-28 05:12:28 +0500

    security ch17

--- books/models.py
+++ books/models.py
@@ -14,9 +14,7 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='covers/', blank=True)
 
     class Meta:
-        indexes = [
-            models.Index(fields=['id'], name='id_index'),
-        ]
+        indexes = [models.Index(fields=['id'], name='id_index'),]
         permissions = [
             ('special_status', 'Can read all books'),
         ]
